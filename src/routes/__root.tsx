ddcd62d4e7038f1fa78614b312bf969e9d2885b6
Louis <louis.dumont@viacesi.fr>
2024-06-07 09:14:58 +0200

    remove unnessery lib

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,4 @@
-import ContentLayout from "@/components/layout/contentLayout";
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
