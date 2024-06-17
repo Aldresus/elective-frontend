53edfeb2e23edddf415d2630ab410d0a1f290f47
Zepyyy <quentin.stubecki@viacesi.fr>
2024-06-17 22:26:17 +0200

    disconnect button (temp) + logged user check function

--- src/routes/_user.tsx
+++ src/routes/_user.tsx
@@ -1,8 +1,8 @@
 import Footer from "@/components/common/footer";
 import Navbar from "@/components/common/navbar";
 import { LoginBanner } from "@/components/user/loginBanner";
+import { useAuth } from "@/lib/auth";
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router";
-import { useState } from "react";
 
 export const Route = createFileRoute("/_user")({
   beforeLoad: ({ context, location }) => {
@@ -19,15 +19,15 @@ export const Route = createFileRoute("/_user")({
 });
 
 function UserLayout() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const auth = useAuth();
   return (
     <div className="bg-yellow-500 min-h-screen">
       <Navbar className="fixed top-0 left-0 w-full" />
       <div className="bg-red-50 mt-[50px] p-9 w-[1280px] mx-auto">
         <LoginBanner
           className="mb-6"
-          hidden={!isLoggedIn}
-          onclose={() => setIsLoggedIn(false)}
+          hidden={auth.isAuthenticated}
+          onclose={() => auth.logout()}
         />
         <Outlet />
       </div>
