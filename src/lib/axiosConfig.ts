15eb9ae22da36404071ff63cc3cbd5cd01639377
Aldresus <aldresusgames@gmail.com>
2024-06-18 21:19:22 +0200

    disclaimer

--- src/lib/axiosConfig.ts
+++ src/lib/axiosConfig.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\
+// /!\    only used for our apis, not for external apis    /!\
+// /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\ /!\
+
 const axiosInstance = axios.create({
   baseURL: "http://middleware.lihoco.fr/api",
   timeout: 1000,
